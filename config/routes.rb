db49bd0b6ede9e9a126ff88b698e9caa16463757
EC2 Default User <ec2-user@ip-172-31-80-60.ec2.internal>
2022-03-18 17:03:10 +0000

    会員登録・編集・マイページ

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,9 @@ Rails.application.routes.draw do
     #resources :sessions, only: [:new, :create, :destroy]
     #resources :registrations, only: [:new, :create]
     get '/about' => 'homes#about'
+    get 'customer/withdraw'
     root to: 'homes#top'
+    get '/customers/my_page' => 'customers#show', as: 'my_page'
   end
 
  # 顧客用

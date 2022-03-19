ce1567e8fe90a876317f92d0d9dbd04f140a9760
EC2 Default User <ec2-user@ip-172-31-14-198.ec2.internal>
2022-03-19 04:48:51 +0000

    [fix]

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -10,7 +10,7 @@ class Admin::CustomersController < ApplicationController
   def update
     @customers = Customer.find(params[:id])
     if @customers.update(customer_params)
-      redirect_to admin_customer_path(@customer)
+      redirect_to admin_customer_path(@customers)
     else
       render "edit"
     end

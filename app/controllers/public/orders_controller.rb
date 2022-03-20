1f742a43efe5ca945040ec11adef2015f13c5505
EC2 Default User <ec2-user@ip-172-31-16-149.ec2.internal>
2022-03-20 14:28:24 +0000

    注文履歴、注文履歴詳細画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -9,10 +9,6 @@ class Public::OrdersController < ApplicationController
     @customer = current_customer
     @order = Order.find(params[:id])
     @order_details = @order.order_details
-<<<<<<< HEAD
-  
-=======
   end
->>>>>>> refs/remotes/origin/orders_show
   
 end

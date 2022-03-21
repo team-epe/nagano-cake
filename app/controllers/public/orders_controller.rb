b76d64b8b7bc5942246d0eee56f5bc8c912aa296
EC2 Default User <ec2-user@ip-172-31-14-198.ec2.internal>
2022-03-21 10:39:19 +0000

    order_function

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,30 +1,57 @@
 class Public::OrdersController < ApplicationController
+  before_action :authenticate_customer!
+
   def new
     @order= Order.new
   end
 
   def create
-    @order = Order.new(order_params)
+    cart_items = current_customer.cart_items.all
+    @order = current_customer.orders.new(order_params)
+
+    if @order.save
+      cart_items.each do |cart|
+        order_detail = OrderDetail.new
+        order_detail.item_id = cart.item_id
+        order_detail.order_id = @order.id
+        order_detail.amount = cart.quantity
+        order_detail.price = cart.item.price
+        order_item.save
+      end
+      redirect_to complete_orders_path
+      cart_items.destroy_all
+    else
+      @order = Order.new(order_params)
+      render :new
+    end
+
+
   end
 
   def confirm
-    if params[:order][:address_select] == "0"
+    @order = Order.new(order_params)
+    @cart_products = current_customer.cart_products
+
+    @order.shipping_cost = 1000
+    @cart_itrems = current_customer.cart_items
+
+    if params[:order][:address_number] == "0"
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
-      @order.name = current_customer.name
-    elsif params[:order][:address_select] == "1"
-      @order.postal_code =  ShipAddress.find(params[:order][:address_id]).postal_code
-      @order.address = ShipAddress.find(params[:order][:address_id]).address
-      @order.name = ShipAddress.find(params[:order][:address_id]).name
-    elsif params[:order][:address_select] == "2"
+      @order.name = current_customer.last_name+current_customer.first_name
+    elsif params[:order][:address_number] == "1"
+      @order.postal_code =  Address.find(params[:order][:address_id]).postal_code
+      @order.address = Address.find(params[:order][:address_id]).address
+      @order.name = Address.find(params[:order][:address_id]).name
+    elsif params[:order][:address_number] == "2"
       @order.postal_code =  params[:order][:postal_code]
       @order.address = params[:order][:address]
       @order.name = params[:order][:name]
     end
-
+    #配送情報がなし
     unless @order.postal_code == "" || @order.address == "" || @order.name == ""
       render :confirm
-
+    #配送情報あり
     else
       @order =Order.new
       @address = current_customer.address
@@ -35,5 +62,9 @@ class Public::OrdersController < ApplicationController
   private
   def order_params
       params.require(:order).permit(:customer_id, :payment_method, :postal_code, :address, :name, :total_payment, :status, :shipping_cost)
+  end
+
+  def address_params
+  params.require(:order).permit(:name, :address)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  has_many :cart_items
+  has_many :orders_details
   
   
   ## 消費税を求めるメソッド

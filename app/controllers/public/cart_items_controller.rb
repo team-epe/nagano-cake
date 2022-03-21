59fe97f9d5848eec110bfbc67e700cf0b806b314
EC2 Default User <ec2-user@ip-172-31-80-60.ec2.internal>
2022-03-21 07:30:29 +0000

    会員退会機能・ステータス変更

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,16 +1,39 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = current_customer.cart_items
+    @total = @cart_items.inject(0) { |sum, item| sum + item }
   end
 
   def create
-    @cart_item=CartItem.new(cart_item_params)
-    @cart_item.save
-    redirect_to public_cart_items_path
+    @cart_item = current_customer.cart_items.find_by(item_id: params[:item_id])
+
+    if @cart_item.present?
+      @cart_item.amount += params[:amount].to_i
+    else
+      @cart_item = current_customer.cart_items.new(cart_item_params)
+    end
+
+    if @cart_item.save
+      redirect_to public_cart_items_path, notice: '商品を追加しました'
+    else
+      redirect_to request.referer
+    end
+
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def destroy_all
   end
 
   private
+
   def cart_item_params
-    params.require(:cart_item).permit(:amount, :customer_id)
+    params.permit(:item_id, :amount)
   end
 
 end

b06255433715a9f921f28a7ae8cb5c13fda7a326
EC2 Default User <ec2-user@ip-172-31-16-149.ec2.internal>
2022-03-24 21:02:57 +0000

    微調整

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,7 +9,13 @@ class Order < ApplicationRecord
     credit_card: 0,
     transfer: 1
   }
-  enum status: { waiting: 0, confirmation: 1, making: 2, preparation: 3, shipped: 4 }
+  enum status: { 
+    waiting: 0,
+    confirmation: 1,
+    making: 2,
+    preparation: 3,
+    shipped: 4
+  }
   
 end
 
